6d283465beb36058afdb9718f23d2c184e7f4294
vamshi krishna prime <vamshi.krishna.prime@gmail.com>
2019-06-17 23:57:33 +0530

    Create date_time.py

---
+++ date_time.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import time
+ti=str(time.ctime())
+dt=str(datetime.today())
+Da=ti[0:3]
+Y=dt[0:4]
+M=dt[6:7]
+nM=['Jan','Feb','Mar','Apr','May','Jun','jul','Aug','Sep','Oct','Nov','Dec'][int(M)-1] or ti[4:7]
+D=dt[8:10]
+h=int(dt[10:13])+1
+if h>12 :
+    h-=12
+    p_a='P.M'
+else :
+    p_a='A.M'
+m_s=dt[14:19]
+# print("%s, %s %s, %s, %d:%s %s"%(Da,nM,Y,D,h,m_s,p_a) or "FAILED TO FIND TIME AND DATE :(")
+print(Da,nM,Y,D,h,m_s,p_a) or "FAILED TO FIND TIME AND DATE :()"
